15bf5cc1306d6ed2fb9519c22fef7ab94464ff35
lyndon <al_la@sobudo.com>
2018-08-09 20:54:18 +1200

    rudimentary exotic characters handling (deletion -_____-)

--- script.sh
+++ script.sh
@@ -2,12 +2,12 @@
 #set -e
 
   #options for ffmpeg
-first_options="-y -hide_banner -v quiet"
+first_options="-y -hide_banner -v error"
 in_options=""
 out_options="-filter aresample=44100 -acodec pcm_s16le"
 
   #path to your netmdcli binary
-netmdcli=../../projects/linux-minidisc-fixes/linux-minidisc/netmdcli/netmdcli
+netmdcli=/home/lyndon/projects/linux-minidisc-fixes/linux-minidisc/netmdcli/netmdcli
 
  #path to your temp file (/tmp/MDtemp.wav)
 temp=/tmp/MDtemp.wav
@@ -22,7 +22,7 @@ tvar=" "
 for file in "$@"; do
 tvar=""
   if [ -f "$file" ]; then 
-    read -r tvar <<< $(ffprobe -v error -show_entries stream=codec_type -select_streams a -of default=noprint_wrappers=1:nokey=1 "$file")
+    read -r tvar <<< $(ffprobe -v quiet -show_entries stream=codec_type -select_streams a -of default=noprint_wrappers=1:nokey=1 "$file")
   fi
   if [ "$tvar" = "audio" ]; then
     list[$iter]=$file
@@ -35,20 +35,24 @@ echo "${#list[@]} files in list."
 echo -e "\n"
 
   #get Artist and Album titles from first item on the list.
-mapfile -t deets  <<<$(ffprobe -v quiet -show_entries format_tags=artist,album -of default=nw=1:nk=1 "${list[0]}")
+r_artist=$(ffprobe -v quiet -show_entries format_tags=artist -of default=nw=1:nk=1 "${list[0]}")
+r_album=$(ffprobe -v quiet -show_entries format_tags=album -of default=nw=1:nk=1 "${list[0]}")
+artist=$(echo "$r_artist" | tr -dc '[:print:]')
+album=$(echo "$r_album" | tr -dc '[:print:]')
 
   #for each item in list, get the title ready for upload and convert the file to PCM16/44k1 then download
 for source in "${list[@]}"; do
-  base=$(echo $source | cut -d "." -f1)
-  title=$(ffprobe -v quiet -show_entries format_tags=title -of default=nw=1:nk=1 "$source")
+#  base=$(echo $source | cut -d "." -f1)
+  r_title=$(ffprobe -v quiet -show_entries format_tags=title -of default=nw=1:nk=1 "$source")
+  title=$(echo "$r_title" | tr -dc '[:print:]')
   echo "Converting: ${source}, ${title}..."
 
   # Do the conversion
-  ffmpeg $first_options $in_options -i $source $out_options $temp
+  ffmpeg $first_options $in_options -i "$source" $out_options "$temp"
   
   ######download using netmdcli , /tmp/MDtemp.wav  and $title
   echo "uploading to MD recorder..."
-  $netmdcli -v send $temp "$title"
+  $netmdcli -v send "$temp" "$title"
   sleep 1 
 done
 
@@ -57,7 +61,10 @@ $netmdcli settitle "$artist - $album"
 echo -e "\n"
 
 # this line is useless sometimes because of the mapfile issue
-echo "Completed ${deets[0]} - ${deets[1]}"
+echo "Completed $artist - ${album}"
 
 echo "Deleting temp file $temp"
 rm $temp
+
+#read the disc contents as confirmation. yay~
+$netmdcli
