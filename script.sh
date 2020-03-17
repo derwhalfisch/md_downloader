5f1b7b9349342ecd1a2557cf3e2d54bb429379c6
lyndon <al_la@sobudo.com>
2020-03-17 18:34:40 +1300

    added time summary and more stability fixes

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ in_options=""
 out_options="-filter aresample=44100 -acodec pcm_s16le"
 
   #path to your netmdcli binary
-netmdcli=/home/you/projects/linux-minidisc-fixes/linux-minidisc/netmdcli/netmdcli
+netmdcli=/home/you/projects/linux-minidisc-fixes/linux-minidisc/netmdcli/netmdcli #-v -t
 
  #path to your temp file (/tmp/MDtemp.wav)
 temp=/tmp/MDtemp.wav
@@ -34,13 +34,27 @@ done
 echo "${#list[@]} files in list."
 echo -e "\n"
 
-  #get Artist and Album titles from first item on the list. mapfile to set both is fucky sometimes
+##get Artist and Album titles from first item on the list. mapfile to set both is fucky sometimes
 r_artist=$(ffprobe -v quiet -show_entries format_tags=artist -of default=nw=1:nk=1 "${list[0]}")
 r_album=$(ffprobe -v quiet -show_entries format_tags=album -of default=nw=1:nk=1 "${list[0]}")
 artist=$(echo "$r_artist" | tr -dc '[:print:]')
 album=$(echo "$r_album" | tr -dc '[:print:]')
 
-  #for each item in list, get the title ready for upload and convert the file to PCM16/44k1 then download
+##aggregate total audio length of the list
+ duration=0
+ tot_duration=0
+for source in "${list[@]}"; do
+  duration=$(ffprobe -v quiet -show_entries format=duration -of default=nw=1:nk=1 "$source")
+  title=$(ffprobe -v quiet -show_entries format_tags=title -of default=nw=1:nk=1 "$source")
+  tot_duration=$(echo "$tot_duration + $duration" | bc)
+  echo "${title} ${duration}"
+done
+echo "Total Duration is: " $(echo "$tot_duration / 60" | bc ) minutes: $tot_duration in seconds.
+echo "Get your disc ready"
+read -n 1 -s -r -p "Press any key to continue"
+
+
+##for each item in list, get the title ready for upload and convert the file to PCM16/44k1 then download
 for source in "${list[@]}"; do
 #  base=$(echo $source | cut -d "." -f1)
   r_title=$(ffprobe -v quiet -show_entries format_tags=title -of default=nw=1:nk=1 "$source")
@@ -52,8 +66,9 @@ for source in "${list[@]}"; do
   
   ######download using netmdcli , /tmp/MDtemp.wav  and $title
   echo "uploading to MD recorder..."
-  $netmdcli -v send "$temp" "$title"
-  sleep 1 
+  $netmdcli send "$temp" "$title"
+  echo "Sleeping for 10..."
+  sleep 10
 done
 
 #set the disk title with netmdcli
